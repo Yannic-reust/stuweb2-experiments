ed8cf9db62efec3647809f02f7274139a5686d19
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-03-04 17:09:01 +0100

    feat: add backend

--- tic_tac_toe/src/socketService.js
+++ tic_tac_toe/src/socketService.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080'); // Replace with your server URL
+const socket = io('https://tictactoe.bbmit-service.tech'); // Replace with your server URL
 
 export default socket;
