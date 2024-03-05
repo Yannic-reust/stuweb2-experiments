6d35e347949e14771b5601fba91d4ea47c20fcc7
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-03-05 13:32:26 +0100

    feat: add acc code

--- three-globe/src/App.jsx
+++ three-globe/src/App.jsx
@@ -1,19 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Globe from './components/Globe'
-import './App.css'
+import { useRef, useMemo,useEffect } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Stats } from '@react-three/drei'
+import useKeyboard from './useKeyboard'
+import { Vector3, Quaternion } from 'three'
 
-function App() {
-  const [count, setCount] = useState(0)
+useEffect(() => {
+  const acl = new Accelerometer({ frequency: 60 });
+
+  const handleReading = () => {
+    console.log("Acceleration along the X-axis " + acl.x);
+    console.log("Acceleration along the Y-axis " + acl.y);
+    console.log("Acceleration along the Z-axis " + acl.z);
+  };
+
+  acl.addEventListener('reading', handleReading);
+  acl.start();
+
+  // Cleanup when component unmounts
+  return () => {
+    acl.removeEventListener('reading', handleReading);
+    acl.stop();
+  };
+}, []);
+
+
+function Ball({ floor }) {
+  console.log('creating ball')
+  const ref = useRef()
+  const keyMap = useKeyboard()
+
+  const v0 = useMemo(() => new Vector3(), [])
+  const q = useMemo(() => new Quaternion(), [])
+  const angularVelocity = useMemo(() => new Vector3(), [])
+
+  useFrame((_, delta) => {
+    keyMap['KeyW'] && (angularVelocity.x -= delta * 5)
+    keyMap['KeyS'] && (angularVelocity.x += delta * 5)
+    keyMap['KeyA'] && (angularVelocity.z += delta * 5)
+    keyMap['KeyD'] && (angularVelocity.z -= delta * 5)
+
+    q.setFromAxisAngle(angularVelocity, delta).normalize()
+    ref.current.applyQuaternion(q)
+
+    angularVelocity.lerp(v0, 0.01) // slow down the roll
+
+    floor.current.position.x += angularVelocity.z * delta
+    floor.current.position.z -= angularVelocity.x * delta
+
+    floor.current.position.x = floor.current.position.x % 10
+    floor.current.position.z = floor.current.position.z % 10
+  })
 
   return (
-    <>
-      <div>
-        <Globe />
-      </div>
-    </>
+    <mesh ref={ref} position-y={1.0}>
+      <sphereGeometry />
+      <meshNormalMaterial wireframe />
+    </mesh>
   )
 }
 
-export default App
+export default function App() {
+  const ref = useRef()
+  return (
+    <Canvas
+      camera={{ position: [0, 2.5, 2.5] }}
+      onCreated={({ camera }) => camera.lookAt(0, 1, 0)}>
+      <gridHelper ref={ref} args={[100, 100]} />
+      <Ball floor={ref} />
+      <Stats />
+    </Canvas>
+  )
+}
